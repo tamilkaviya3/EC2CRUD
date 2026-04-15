7deab55cc5c14ca28265a75388cf77faaa362830
tamilkaviya <121109168+tamilkaviya3@users.noreply.github.com>
2026-04-15 13:54:28 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,13 @@
+<?php
+$servername = "testpep.c32exyjbusk5.ap-south-1.rds.amazonaws.com";
+$username = "admin";
+$password = "PFuPWrGANwy90cvovImR";
+$dbname = "testvk";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
